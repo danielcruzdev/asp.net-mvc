fb614f4b24b70038508dd28479252df83fae72fc
Daniel Antonio de França Cruz <daniel.cruz@wscorp.com.br>
2020-06-18 16:01:55 -0300

    Learning about MVC Routes and Bundle

---
+++ OdeToFood/OdeToFood.Data/Models/CuisineType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Models
+{
+    public enum CuisineType
+    {
+        None, 
+        Italian,
+        Indian,
+        French,
+        American,
+        Brazilian,
+        Mexican,
+        Chinese,
+        Japanse
+    }
+}
---
+++ OdeToFood/OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,28 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Daniel's Burguer", Cuisine = CuisineType.American },
+                new Restaurant { Id = 2, Name = "Daniel's Pizza", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 3, Name = "Daniel's Pasta", Cuisine = CuisineType.French }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(restaurant => restaurant.Name);
+        }
+    }
+}
