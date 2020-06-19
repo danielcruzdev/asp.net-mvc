b8765ae1ca6e69a096674da9cd836e13fca5485a
Daniel Cruz <daniel.cruz@writesys.onmicrosoft.com>
2020-06-19 11:18:32 -0300

    Set AutoFac Container Manager

--- OdeToFood/OdeToFood.Web/Global.asax.cs
+++ OdeToFood/OdeToFood.Web/Global.asax.cs
@@ -12,10 +12,12 @@ namespace OdeToFood.Web
     {
         protected void Application_Start()
         {
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ContainerConfig.RegisteContainer();
         }
     }
 }
